2f3354549719fdc982513c2f2285eb8f7a6804d2
Tom Hinton <tomjhinton@gmail.com>
2021-02-24 08:40:19 +0000

    basic color shaders

--- src/app.js
+++ src/app.js
@@ -1,2 +1,3 @@
 import './style.scss'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
--- src/shaders/floor/fragmentFloor.glsl
+++ src/shaders/floor/fragmentFloor.glsl
@@ -14,6 +14,6 @@ void main()
 
 
 
-gl_FragColor = vec4(0.4, 0.2, 0.4, 0.4);
+gl_FragColor = vec4(0.4, 0.6, 0.4, 0.4);
 
 }
